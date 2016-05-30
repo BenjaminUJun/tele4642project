7c74c14401be237f79c61823438d2a265facbc50
spectrogram <addie@Adelines-MBP.home.gateway>
2016-05-30 17:22:40 +1000

    Still not working
    Unsupported OF request in this version: 4
    Traceback (most recent call last):
      File "/Library/Python/2.7/site-packages/ryu/app/ofctl_rest.py", line
    274, in wrapper
        method(self, req, dp, ofctl, body, *args, **kwargs)
      File "/Library/Python/2.7/site-packages/ryu/app/ofctl_rest.py", line
    430, in mod_flow_entry
        ofctl.mod_flow_entry(dp, flow, mod_cmd)
      File "/Library/Python/2.7/site-packages/ryu/lib/ofctl_v1_3.py", line
    1054, in mod_flow_entry
        match = to_match(dp, flow.get('match', {}))
      File "/Library/Python/2.7/site-packages/ryu/lib/ofctl_v1_3.py", line
    252, in to_match
        if attrs.get('dl_type') == ether.ETH_TYPE_ARP or \

--- App.py
+++ App.py
@@ -1,21 +1,3 @@
-from ryu.base import app_manager
-from ryu.controller import ofp_event
-from ryu.controller import dpset
-from ryu.controller.handler import MAIN_DISPATCHER
-from ryu.controller.handler import set_ev_cls
-from ryu.exception import RyuException
-from ryu.ofproto import ofproto_v1_0
-from ryu.ofproto import ofproto_v1_2
-from ryu.ofproto import ofproto_v1_3
-from ryu.ofproto import ofproto_v1_4
-from ryu.ofproto import ofproto_v1_5
-from ryu.lib import ofctl_v1_0
-from ryu.lib import ofctl_v1_2
-from ryu.lib import ofctl_v1_3
-from ryu.lib import ofctl_v1_4
-from ryu.lib import ofctl_v1_5
-from ryu.app.wsgi import ControllerBase, WSGIApplication
-
 import requests
 import re
 import json
@@ -24,7 +6,6 @@ devices = requests.get('http://localhost:8080/stats/switches')
 deviceList = re.findall("([\d]+)", devices.text)
 
 iplist = []
-
 
 # parse the list of Netflix IPs and put them into the list
 def getIPs():
@@ -44,6 +25,6 @@ for device in deviceList:
    response = requests.post('http://localhost:8080/stats/flowentry/add', data=initFlow2) 
 
    for ip in iplist:
-      netflixFlow = "{\"dpid\": %s, \"table_id\": 0, \"idle_timeout\": 0, \"hard_timeout\": 0, \"priority\": 2, \"flags\": 1, \"match\":[ { \"in_port\":1 }, { \"nw_src\": \"%s\",  \"dl_type\": 2048 } ], \"actions\":[ { \"type\":\"OUTPUT\", \"port\": 2 } ] }" % (device, ip)
+      netflixFlow = "{\"dpid\": %s, \"table_id\": 0, \"idle_timeout\": 0, \"hard_timeout\": 0, \"priority\": 2, \"flags\": 1, \"match\":[ { \"in_port\":1 }, { \"ipv4_src\": \"%s\",  \"eth_type\": 2048 } ], \"actions\":[ { \"type\":\"OUTPUT\", \"port\": 2 } ] }" % (device, ip)
       response = requests.post('http://localhost:8080/stats/flowentry/add', data=netflixFlow)
       break
