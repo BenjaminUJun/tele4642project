e772e77733a9b5a76022adae5a0a48311318e28d
spectrogram <addie@Adelines-MBP.home.gateway>
2016-05-30 16:30:19 +1000

    Corrected the syntax of the Netflix flow
    Still doesn’t work

--- App.py
+++ App.py
@@ -1,3 +1,21 @@
+from ryu.base import app_manager
+from ryu.controller import ofp_event
+from ryu.controller import dpset
+from ryu.controller.handler import MAIN_DISPATCHER
+from ryu.controller.handler import set_ev_cls
+from ryu.exception import RyuException
+from ryu.ofproto import ofproto_v1_0
+from ryu.ofproto import ofproto_v1_2
+from ryu.ofproto import ofproto_v1_3
+from ryu.ofproto import ofproto_v1_4
+from ryu.ofproto import ofproto_v1_5
+from ryu.lib import ofctl_v1_0
+from ryu.lib import ofctl_v1_2
+from ryu.lib import ofctl_v1_3
+from ryu.lib import ofctl_v1_4
+from ryu.lib import ofctl_v1_5
+from ryu.app.wsgi import ControllerBase, WSGIApplication
+
 import requests
 import re
 import json
@@ -12,7 +30,7 @@ iplist = []
 def getIPs():
    with open("iplist.txt", "r") as input:
     for line in input:
-        iplist.append(line.strip())
+        iplist.append(unicode(line.strip()))
 
 getIPs()
 
@@ -26,5 +44,6 @@ for device in deviceList:
    response = requests.post('http://localhost:8080/stats/flowentry/add', data=initFlow2) 
 
    for ip in iplist:
-      netflixFlow = "{\"dpid\": %s, \"table_id\": 0, \"idle_timeout\": 0, \"hard_timeout\": 0, \"priority\": 2, \"flags\": 1, \"match\":{ \"in_port\":1, \"nw_src\": \"%s\",  \"dl_type\": 2048 }, \"actions\":[ \"type\":\"OUTPUT\", \"port\": 2 ]}" % (device, ip)
+      netflixFlow = "{\"dpid\": %s, \"table_id\": 0, \"idle_timeout\": 0, \"hard_timeout\": 0, \"priority\": 2, \"flags\": 1, \"match\":[ { \"in_port\":1 }, { \"nw_src\": \"%s\",  \"dl_type\": 2048 } ], \"actions\":[ { \"type\":\"OUTPUT\", \"port\": 2 } ] }" % (device, ip)
       response = requests.post('http://localhost:8080/stats/flowentry/add', data=netflixFlow)
+      break
