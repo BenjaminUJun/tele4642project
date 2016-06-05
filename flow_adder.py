f1f0a0aebb9a3a6c460a192e9884e45e50525e42
spectrogram <addie@Adelines-MBP.home.gateway>
2016-06-05 22:45:54 +1000

    Removed redundant imports

--- flow_adder.py
+++ flow_adder.py
@@ -7,14 +7,11 @@ from ryu.base import app_manager
 from ryu.controller import ofp_event
 from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER, DEAD_DISPATCHER
 from ryu.controller.handler import set_ev_cls
-from ryu.ofproto import ofproto_v1_3
 from ryu.lib.packet import packet
 from ryu.lib.packet import ethernet
 from ryu.lib.packet import ether_types
 from ryu.lib.packet import arp, ipv4
 from ryu.lib import hub
-import threading
-import array
 import ipaddress
 
 # list of Netflix IP ranges
